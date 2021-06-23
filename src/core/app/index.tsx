54b4da48765073fc26d83c4b6c769f9964e90549
nmhieudut <hieutk5@gmail.com>
2021-06-23 11:50:09 +0700

    Restore cache of gitignore

--- src/core/app/index.tsx
+++ src/core/app/index.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import store from 'store'
 import { Provider } from 'react-redux'
 import { Layout } from 'core/layout'
-import {withTranslation} from "react-i18next"
+import { withTranslation, I18nextProvider } from 'react-i18next'
 import i18n from 'plugins/i18n'
-import { I18nextProvider } from 'react-i18next'
 require('dotenv').config()
 
 const App: React.FC = () => {
-  console.log("======> App is on:", process.env.NODE_ENV)
+  console.log('======> App is on', process.env.NODE_ENV)
   return (
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
