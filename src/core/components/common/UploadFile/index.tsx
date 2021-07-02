90aabd39c8bc3ee0e5899bc5e6dc1401aafa0ab8
Hieu Nguyen <hieutk5@gmail.com>
2021-07-02 22:42:42 +0700

    Building: Add responsive navbar

---
+++ src/core/components/common/UploadFile/index.tsx
@@ -0,0 +1,13 @@
+interface Props {
+     
+}
+
+const UploadFile = (props: Props) => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
+
+export default UploadFile
--- src/core/layout/Header/index.tsx
+++ src/core/layout/Header/index.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { SignInAction } from 'store/auth'
 import { RootState } from 'store/reducers'
 import { navMenu } from 'constants/navbar'
 import t from 'core/utils/translation'
+import { Divide as Hamburger } from 'hamburger-react'
 export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
+  const [showLinks, setShowLinks] = useState(false)
   const dispatch = useDispatch()
   // const isLoading = useSelector((state: RootState) => state.auth.isLoading)
   const onLogin = () => {
@@ -22,26 +25,38 @@ export default function Header(props: IHeaderProps) {
       <Link to="#" className="header-logo mr-24">
         Logo
       </Link>
-      <div className="flex justify-center items-center ml-auto">
-        <Link className="menu-item" to="/home">
-          {t('content.menu.home')}
-        </Link>
-        {navMenu.map((item, i) => {
-          return (
-            <Link key={i} className="menu-item ml-20" to={item.page_path}>
-              {t(`content.menu.${item.children}`)}
-            </Link>
-          )
-        })}
+      <div
+        className="flex justify-center items-center ml-auto menu-collapse"
+        id={showLinks ? 'hidden' : ''}
+      >
+        <div className="flex menu-collapse-links">
+          <Link className="menu-item" to="/home">
+            {t('content.menu.home')}
+          </Link>
+          {navMenu.map((item, i) => {
+            return (
+              <Link key={i} className="menu-item ml-14" to={item.page_path}>
+                {t(`content.menu.${item.children}`)}
+              </Link>
+            )
+          })}
+        </div>
+        <div className="auth-menu">
+          <Link onClick={onLogin} className="btn btn-transparent" to="#">
+            {t('content.menu.login')}
+          </Link>
+          <Link className="btn btn-primary" to="#">
+            {t('content.menu.signup')}
+          </Link>
+        </div>
       </div>
-      <div className="auth-menu">
-        <Link onClick={onLogin} className="btn btn-transparent" to="#">
-          {t('content.menu.login')}
-        </Link>
-        <Link className="btn btn-primary" to="#">
-          {t('content.menu.signup')}
-        </Link>
-      </div>
+      <button
+        className="ml-auto close-collapse-icon"
+        onClick={() => setShowLinks(!showLinks)}
+      >
+        <Hamburger direction="right" />
+      </button>
+      
     </div>
   )
 }
